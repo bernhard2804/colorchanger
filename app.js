8f27602f1e3db723c33f9e3f056dd616e51297bc
Berhard Leube <bernhard@vlek.co>
2020-10-02 16:57:14 +0200

    1 sekunde

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ function randomRGB(){
 const letters = document.querySelectorAll('.letter');
 const intervalId = setInterval(function(){
     for (let letter of letters){
-        letter.getElementsByClassName.color = randomRGB();
+        letter.style.color = randomRGB();
     }
-}, 2000);
+}, 1000);
